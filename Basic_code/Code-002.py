151885bb887cefa9950d5b3467fdad92e8cbd44d
kundan kumar <119260880+kundankumarnrg@users.noreply.github.com>
2023-03-23 15:11:11 +0530

    Add files via upload

---
+++ Basic_code/Code-002.py
@@ -0,0 +1,13 @@
+
+
+lst=[11,21,31,4,5,6]
+"""
+def multiply_list_element(lst):
+    mul=1
+    for val in lst:
+        mul=mul*val
+    print("Multiply list elements: ",mul)
+       
+multiply_list_element(lst)
+"""
+
